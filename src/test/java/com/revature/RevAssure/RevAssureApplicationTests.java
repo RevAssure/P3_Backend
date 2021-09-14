c199b99a04c8ff283f85cedf0b61b89bea9daca0
DDRIV <ddriv4@gmail.com>
2021-09-14 13:31:23 -0500

    initial commit

---
+++ src/test/java/com/revature/RevAssure/RevAssureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.RevAssure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevAssureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
