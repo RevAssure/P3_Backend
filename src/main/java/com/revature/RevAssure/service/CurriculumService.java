38640720087a3c6036673d8d3dfc902539019e74
cptvictor <victorsyanwenliu19@gmail.com>
2021-09-15 17:28:00 -0400

    implemented service methods

--- src/main/java/com/revature/RevAssure/service/CurriculumService.java
+++ src/main/java/com/revature/RevAssure/service/CurriculumService.java
@@ -1,6 +1,9 @@
 package com.revature.RevAssure.service;
 
 import com.revature.RevAssure.model.Curriculum;
+import com.revature.RevAssure.model.RevUser;
+import com.revature.RevAssure.repository.CurriculumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,14 +11,21 @@ import java.util.List;
 @Service
 public class CurriculumService
 {
+    private CurriculumRepository curriculumRepository;
+
+    @Autowired
+    public CurriculumService(CurriculumRepository curriculumRepository)
+    {
+        this.curriculumRepository = curriculumRepository;
+    }
 
     public Curriculum saveCurriculum(Curriculum curriculum)
     {
-        return null;
+        return curriculumRepository.save(curriculum);
     }
 
-    public List<Curriculum> getAllCurriculaByTrainerId()
+    public List<Curriculum> getAllCurriculaByTrainerId(RevUser trainer)
     {
-        return null;
+        return curriculumRepository.findAllByRevUser(trainer);
     }
 }
