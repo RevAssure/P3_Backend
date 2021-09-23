4c2bbd1f8559316cce700153d9ceb4e386308f2f
cptvictor <victorsyanwenliu19@gmail.com>
2021-09-23 01:00:17 -0400

    testing first integration test

--- src/test/java/com/revature/RevAssure/controller/CurriculumControllerTest.java
+++ src/test/java/com/revature/RevAssure/controller/CurriculumControllerTest.java
@@ -1,5 +1,8 @@
 package com.revature.RevAssure.controller;
 
+import com.revature.RevAssure.model.Curriculum;
+import com.revature.RevAssure.model.Event;
+import com.revature.RevAssure.model.RevUser;
 import com.revature.RevAssure.service.CurriculumService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +12,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,10 +31,40 @@ class CurriculumControllerTest {
     private CurriculumController curriculumControllerTest;
 
     @MockBean
-    private CurriculumService curriculumService;
+    private CurriculumService mockCurriculumService;
+
+    private Curriculum mockCurriculum;
+
+    private List<Curriculum> mockCurriculumList;
+
+    private RevUser mockTrainer;
+
+    private RevUser mockAssociate;
+
+    private Event mockEvent;
 
     @BeforeEach
-    void setUp() {
+    void setUp()
+    {
+        mockCurriculum = new Curriculum();
+        mockCurriculum.setId(1);
+        mockCurriculum.setName("test");
+
+        mockTrainer = new RevUser();
+        mockCurriculum.setTrainer(mockTrainer);
+
+        mockEvent = new Event();
+        List<Event> mockEvents = new ArrayList<>();
+        mockEvents.add(mockEvent);
+        mockCurriculum.setEvents(mockEvents);
+
+        mockAssociate = new RevUser();
+        List<RevUser> mockAssociates = new ArrayList<>();
+        mockAssociates.add(mockAssociate);
+        mockCurriculum.setRevUsers(mockAssociates);
+
+        mockCurriculumList = new ArrayList<>();
+        mockCurriculumList.add(mockCurriculum);
     }
 
     @AfterEach
@@ -41,10 +78,13 @@ class CurriculumControllerTest {
     @Test
     void getAllCurriculaByCurrentUserId() throws Exception
     {
+        when(mockCurriculumService.getAllCurriculaByTrainer(mockTrainer)).thenReturn(mockCurriculumList);
+
         mockMvc.perform(get("/curriculum")
                         .contentType("application/json"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$[0]").value(mockCurriculum))
                 .andReturn();
     }
 
