2704af71481d51084e864de05b678bec7fb7ddaf
cptvictor <victorsyanwenliu19@gmail.com>
2021-09-23 02:01:45 -0400

    test foundation complete

--- src/test/java/com/revature/RevAssure/controller/CurriculumControllerTest.java
+++ src/test/java/com/revature/RevAssure/controller/CurriculumControllerTest.java
@@ -4,34 +4,59 @@ import com.revature.RevAssure.model.Curriculum;
 import com.revature.RevAssure.model.Event;
 import com.revature.RevAssure.model.RevUser;
 import com.revature.RevAssure.service.CurriculumService;
+import com.revature.RevAssure.service.RevUserDetailsService;
+import com.revature.RevAssure.service.RevUserService;
+import com.revature.RevAssure.util.JwtUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@SpringBootTest
+//@AutoConfigureMockMvc
 @WebMvcTest(controllers = CurriculumController.class)
-class CurriculumControllerTest {
-
+class CurriculumControllerTest
+{
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
 
     @Autowired
     private CurriculumController curriculumControllerTest;
 
     @MockBean
     private CurriculumService mockCurriculumService;
+
+    @MockBean
+    private RevUserDetailsService mockRevUserDetailsService;
+
+    @MockBean
+    private JwtUtil mockJwtUtil;
+
+    @MockBean
+    private RevUserService mockRevUserService;
 
     private Curriculum mockCurriculum;
 
@@ -46,6 +71,11 @@ class CurriculumControllerTest {
     @BeforeEach
     void setUp()
     {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
         mockCurriculum = new Curriculum();
         mockCurriculum.setId(1);
         mockCurriculum.setName("test");
@@ -71,27 +101,32 @@ class CurriculumControllerTest {
     void tearDown() {
     }
 
+    @WithMockUser
     @Test
     void createCurriculum() {
     }
 
+    @WithMockUser
     @Test
     void getAllCurriculaByCurrentUserId() throws Exception
     {
+        when(mockJwtUtil.extractUser(mockRevUserService)).thenReturn(mockTrainer);
         when(mockCurriculumService.getAllCurriculaByTrainer(mockTrainer)).thenReturn(mockCurriculumList);
 
         mockMvc.perform(get("/curriculum")
                         .contentType("application/json"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isNotEmpty())
-                .andExpect(jsonPath("$[0]").value(mockCurriculum))
+                .andExpect(jsonPath("$[0].id").value(1))
                 .andReturn();
     }
 
+    @WithMockUser
     @Test
     void getAssignedCurriculaByCurrentUserId() {
     }
 
+    @WithMockUser
     @Test
     void updateCurriculum() {
     }
